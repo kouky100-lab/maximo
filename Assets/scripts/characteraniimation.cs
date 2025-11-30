26c613e35d7cf7fb75bf79d0aed3dd66fbd98fc7
youssefkhaled22d <kouky100@gmail.com>
2025-11-30 20:19:28 +0200

    1

---
+++ Assets/scripts/characteraniimation.cs
@@ -0,0 +1,24 @@
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Animator))]
+public class CharacterAnimations : MonoBehaviour
+{
+    public Animator characterAnimator;
+
+    private void Awake()
+    {
+        characterAnimator = GetComponent<Animator>();
+    }
+
+    public void OnJump(InputValue value)
+    {
+        Debug.Log("Jump pressed!");
+
+        if (value.isPressed)
+        {
+            characterAnimator.SetTrigger("Jump");
+        }
+    }
+}
